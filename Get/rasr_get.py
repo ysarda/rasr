e428db8c8c6a3c60fe6cdb848c2dcea2dbba4d8d
RobbyKeh <13708361+RobbyKeh@users.noreply.github.com>
2020-01-31 21:24:27 -0600

    Add files via upload
    Updated rasrGet files for current working scraper

--- Get/rasr_get.py
+++ Get/rasr_get.py
@@ -5,54 +5,30 @@ ver 1.0 as of Feb 01, 2019
 
 See README for details
 
-@author: Benjamin Miller 
+@author: Benjamin Miller and Robby Keh
 """
 import warnings
 with warnings.catch_warnings():
-    warnings.simplefilter("ignore",category=FutureWarning)
+    warnings.simplefilter("ignore", category=FutureWarning)
     warnings.simplefilter("ignore", category=DeprecationWarning)
     import os
     import sys
-    from multiprocessing import Pool, cpu_count 
-    import boto3
-    import datetime
+    from multiprocessing import Pool, cpu_count
+    from datetime import date, datetime, timedelta
+    from getData import runFunction
     from functools import partial
 
-def getData(site, keylist):
-    #https://www.nsstc.uah.edu/users/brian.freitag/AWS_Radar_with_Python.html
-    #Set up Amazon AWS connections for client and environment
-    
-    s3conn = boto3.client(
-        's3',
-        aws_access_key_id= str(keylist[0]),
-        aws_secret_access_key=str(keylist[1])
-    )
-    #use boto to connect to the immediate AWS nexrad bucket list for the current date
-    now = datetime.datetime.utcnow()
-    date = ("{:4d}".format(now.year) + '/' + "{:02d}".format(now.month) + '/' +
-            "{:02d}".format(now.day) + '/'+str(site)+'/')
-    
-    #get the most recent data for the selected site (if site exists)
-    objs = s3conn.list_objects_v2(Bucket='noaa-nexrad-level2',Prefix=date,Delimiter='/')
-    if 'Contents' in objs:
-        last_added=max(objs['Contents'],key=lambda x: x['LastModified'])
-        #check that we dont use _MDM files 
-        if not last_added['Key'].endswith("_MDM"):
-            #Get name
-            fname = last_added['Key']
-            printname = fname.split('/')[-1]
-            printname = printname[0:19]
-            localfile = os.getcwd()+"/tmp/"+printname
-            #Confirm new file
-            if not os.path.exists(localfile):
-                #get file and store to temporary file for analysis (cannot direct load in current version)
-                obj = boto3.resource('s3').Bucket('noaa-nexrad-level2').Object(fname)
-                with open(localfile,'wb') as data:
-                    obj.download_fileobj(data)    
-                #localfile.close() is implicit with "with" structure
-                #os.remove(localfile.name)
-
 if __name__== '__main__':
+
+    # Configurable Parameters
+    now = datetime.now()
+    today = datetime.today()
+    yesterday = today - timedelta(1)
+
+    # Get the most updated values from yesterday
+    dateList = [yesterday.year, yesterday.month, yesterday.day, now.year, now.month, now.day]
+    timerange = [0, 235959]
+
     #optional spec for num pool workers, else num cpu
     if len(sys.argv)>1:
         runnum = sys.argv[1]
@@ -73,24 +49,35 @@ if __name__== '__main__':
     
     #Create storage directory 
     dirname = os.getcwd()+"/tmp"
-    if not os.path.exists(dirname):                 
-        try:
-                os.mkdir(dirname)
-        except FileExistsError:
-                pass
-        
-    with open("awskeys.txt","r") as keyfile:
-        lines = keyfile.read().splitlines()
-    #lines = file.readlines()
-    keyfile.close()
-    os.environ["AWS_ACCESS_KEY_ID"] = str(lines[0])
-    os.environ["AWS_SECRET_ACCESS_KEY"] = str(lines[1])
-    
-    
-    pool = Pool(processes=int(runnum)) #Pool(processes=#)
-    getDataPart = partial(getData, keylist=lines)
-    pool.map(getDataPart, sites)
 
+    if not os.path.exists(dirname):
+        try:
+            os.mkdir(dirname)
+            # os.chdir(dirname)
+            # f = open('data_links.txt', 'wb')
+        except FileExistsError:
+            os.chdir(dirname)
+            if os.path.exists('data_links.txt'):
+                pass
+            else:
+                f = open('data_links.txt', 'wb')
+
+    static_dir = os.getcwd() + '//tmp'
+
+    # runFunction(dateList, timerange, sites, static_dir)
+
+    if len(sys.argv) > 1:
+        runnum = sys.argv[1]
+    else:
+        runnum = cpu_count()
+
+    pool = Pool(processes=int(runnum))  # Pool(processes=#)
+    # runFunctionPart = partial(runFunction)
+    runFunctionPart = partial(runFunction, dateList=dateList, timerange=timerange, static_dir=static_dir)
+    # pool.map(runFunction(dateList, timerange, static_dir, sites), iterable=sites)
+    pool.map(runFunctionPart, sites)
+
+    print('Done')
 
 
 
--- Get/rasr_get_test.py
+++ Get/rasr_get_test.py
@@ -5,94 +5,101 @@ ver 1.0 as of Feb 01, 2019
 
 See README for details
 
-@author: Benjamin Miller 
+@author: Benjamin Miller and Robby
 """
+
 import warnings
+
 with warnings.catch_warnings():
-    warnings.simplefilter("ignore",category=FutureWarning)
+    warnings.simplefilter("ignore", category=FutureWarning)
     warnings.simplefilter("ignore", category=DeprecationWarning)
     import os
     import sys
-    from multiprocessing import Pool, cpu_count 
-    import boto3
-    import datetime
+    from multiprocessing import Pool, cpu_count
+    #import boto3
+    from datetime import date, datetime, timedelta
+    from getData import runFunction
     from functools import partial
 
-def getData(site, keylist):
-    #https://www.nsstc.uah.edu/users/brian.freitag/AWS_Radar_with_Python.html
-    #Set up Amazon AWS connections for client and environment
-    
-    s3conn = boto3.client(
-        's3',
-        aws_access_key_id= str(keylist[0]),
-        aws_secret_access_key=str(keylist[1])
-    )
-    #use boto to connect to the immediate AWS nexrad bucket list for the current date
-    #now = datetime.datetime.utcnow()
-    #date = ("{:4d}".format(now.year) + '/' + "{:02d}".format(now.month) + '/' +
-    #        "{:02d}".format(now.day) + '/'+str(site)+'/')
-    date = '2009/02/15/KFWS/'
+if __name__ == '__main__':
 
-    #get the most recent data for the selected site (if site exists)
-    objs = s3conn.list_objects_v2(Bucket='noaa-nexrad-level2',Prefix=date,Delimiter='/')
-    if 'Contents' in objs:
-        last_added=max(objs['Contents'],key=lambda x: x['LastModified'])
-        #check that we dont use _MDM files 
-        if not last_added['Key'].endswith("_MDM"):
-            #Get name
-            fname = '2009/02/15/KFWS/KFWS20090215_165332_V03.gz'
-            #fname = last_added['Key']
-            printname = fname.split('/')[-1]
-            printname = printname[0:19]
-            localfile = os.getcwd()+"/tmp/"+printname
-            #Confirm new file
-            if not os.path.exists(localfile):
-                #get file and store to temporary file for analysis (cannot direct load in current version)
-                obj = boto3.resource('s3').Bucket('noaa-nexrad-level2').Object(fname)
-                with open(localfile,'wb') as data:
-                    obj.download_fileobj(data)    
-                #localfile.close() is implicit with "with" structure
-                #os.remove(localfile.name)
+    # Configurable Parameters
+    now = datetime.now()
+    today = datetime.today()
+    yesterday = today - timedelta(1)
 
-if __name__== '__main__':
-    #optional spec for num pool workers, else num cpu
+    # Get the data for today
+
+    # Default
+    #dateList = [yesterday.year, yesterday.month, yesterday.day, now.year, now.month, now.day]
+    #timerange = [0, 235959]
+
+    # Test
+    dateList = [yesterday.year, yesterday.month, yesterday.day, now.year, now.month, now.day]
+    timerange = [0, 235959]
+
+    # optional spec for num pool workers, else num cpu
+    if len(sys.argv) > 1:
+        runnum = sys.argv[1]
+    else:
+        runnum = cpu_count()
+
+    sites = ['KABR','KENX','KABX','KAMA','PAHG','PGUA','KFFC','KBBX','PABC','KBLX','KBGM','PACG','KBMX','KBIS','KFCX','KCBX','KBOX']
+
+             #'KBRO','KBUF','KCXX','RKSG','KFDX','KCBW','KICX','KGRK','KCLX','KRLX','KCYS','KLOT','KILN','KCLE','KCAE','KGWX',
+             #'KCRP','KFTG','KDMX','KDTX','KDDC','KDOX','KDLH','KDYX','KEYX','KEPZ','KLRX','KBHX','KVWX','PAPD','KFSX','KSRX',
+             #'KFDR','KHPX','KPOE','KEOX','KFWS','KAPX','KGGW','KGLD','KMVX','KGJX','KGRR','KTFX','KGRB','KGSP','KUEX','KHDX',
+             #'KHGX','KHTX','KIND','KJKL','KDGX','KJAX','RODN','PHKM','KEAX','KBYX','PAKC','KMRX','RKJK','KARX','KLCH','KLGX',
+             #'KESX','KDFX','KILX','KLZK','KVTX','KLVX','KLBB','KMQT','KMXX','KMAX','KMLB','KNQA','KAMX','PAIH','KMAF','KMKX',
+             #'KMPX','KMBX','KMSX','KMOB','PHMO','KTYX','KVAX','KMHX','KOHX','KLIX','KOKX','PAEC','KLNX','KIWX','KEVX','KTLX',
+             #'KOAX','KPAH','KPDT','KDIX','KIWA','KPBZ','KSFX','KGYX','KRTX','KPUX','KDVN','KRAX','KUDX','KRGX','KRIW','KJGX',
+             #'KDAX','KMTX','KSJT','KEWX','KNKX','KMUX','KHNX','TJUA','KSOX','KATX','KSHV','KFSD','PHKI','PHWA','KOTX','KSGF',
+             #'KLSX','KCCX','KLWX','KTLH','KTBW','KTWX','KEMX','KINX','KVNX','KVBX','KAKQ','KICT','KLTX','KYUX']
+
+    # Create Storage Directory CHANGE TO BACKSLASHES FOR LINUX
+    dirname = os.getcwd() + "//tmp" # for windows
+
+    if not os.path.exists(dirname):
+        try:
+            os.mkdir(dirname)
+            #os.chdir(dirname)
+            #f = open('data_links.txt', 'wb')
+        except FileExistsError:
+            os.chdir(dirname)
+            if os.path.exists('data_links.txt'):
+                pass
+            else:
+                f = open('data_links.txt', 'wb')
+
+    static_dir = os.getcwd() + '//tmp'
+
+    #runFunction(dateList, timerange, sites, static_dir)
+
     if len(sys.argv)>1:
         runnum = sys.argv[1]
     else:
         runnum = cpu_count()
-        
-    sites = ['KCRP']
-    #sites = ['KABR' ,'KENX','KABX','KAMA','PAHG','PGUA','KFFC','KBBX','PABC','KBLX','KBGM','PACG','KBMX','KBIS','KFCX','KCBX','KBOX',
-#         'KBRO','KBUF','KCXX','RKSG','KFDX','KCBW','KICX','KGRK','KCLX','KRLX','KCYS','KLOT','KILN','KCLE','KCAE','KGWX',
-#         'KCRP','KFTG','KDMX','KDTX','KDDC','KDOX','KDLH','KDYX','KEYX','KEPZ','KLRX','KBHX','KVWX','PAPD','KFSX','KSRX',
-#         'KFDR','KHPX','KPOE','KEOX','KFWS','KAPX','KGGW','KGLD','KMVX','KGJX','KGRR','KTFX','KGRB','KGSP','KUEX','KHDX',
-#         'KHGX','KHTX','KIND','KJKL','KDGX','KJAX','RODN','PHKM','KEAX','KBYX','PAKC','KMRX','RKJK','KARX','KLCH','KLGX',
-#         'KESX','KDFX','KILX','KLZK','KVTX','KLVX','KLBB','KMQT','KMXX','KMAX','KMLB','KNQA','KAMX','PAIH','KMAF','KMKX',
-#         'KMPX','KMBX','KMSX','KMOB','PHMO','KTYX','KVAX','KMHX','KOHX','KLIX','KOKX','PAEC','KLNX','KIWX','KEVX','KTLX',
-#         'KOAX','KPAH','KPDT','KDIX','KIWA','KPBZ','KSFX','KGYX','KRTX','KPUX','KDVN','KRAX','KUDX','KRGX','KRIW','KJGX',
-#         'KDAX','KMTX','KSJT','KEWX','KNKX','KMUX','KHNX','TJUA','KSOX','KATX','KSHV','KFSD','PHKI','PHWA','KOTX','KSGF',
-#         'KLSX','KCCX','KLWX','KTLH','KTBW','KTWX','KEMX','KINX','KVNX','KVBX','KAKQ','KICT','KLTX','KYUX']
 
-    
-    #Create storage directory 
-    dirname = os.getcwd()+"/tmp"
-    if not os.path.exists(dirname):                 
-        try:
-                os.mkdir(dirname)
-        except FileExistsError:
-                pass
-        
-    with open("awskeys.txt","r") as keyfile:
-        lines = keyfile.read().splitlines()
-    #lines = file.readlines()
-    keyfile.close()
-    os.environ["AWS_ACCESS_KEY_ID"] = str(lines[0])
-    os.environ["AWS_SECRET_ACCESS_KEY"] = str(lines[1])
-    
-    
-    pool = Pool(processes=int(runnum)) #Pool(processes=#)
-    getDataPart = partial(getData, keylist=lines)
-    pool.map(getDataPart, sites)
+    pool = Pool(processes=int(runnum))  # Pool(processes=#)
+    #runFunctionPart = partial(runFunction)
+    runFunctionPart = partial(runFunction, dateList=dateList, timerange=timerange, static_dir=static_dir)
+    #pool.map(runFunction(dateList, timerange, static_dir, sites), iterable=sites)
+    pool.map(runFunctionPart, sites)
+
+
+    '''
+    pool = Pool(processes=int(runnum))  # Pool(processes=#)
+    runFunctionPart = partial(runFunction, dateList=dateList, timerange=timerange, static_dir=static_dir)
+    pool.map(runFunctionPart(dateList, timerange, static_dir), iterable=sites)
+    '''
+
+    # consider deleting
+    try:
+        os.remove('data_links.txt')
+    except:
+        pass
+    # Deleting the data.txt?
+    print('Done')
 
 
 
