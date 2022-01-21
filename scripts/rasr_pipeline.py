ebb766c8624c5a4c2133c323bdcf5e09b777daf0
Carson Lansdowne <carsonlansdowne@gmail.com>
2022-01-21 15:32:55 -0600

    Added to pipeline, additional comments (needs to be debugged)

--- scripts/rasr_pipeline.py
+++ scripts/rasr_pipeline.py
@@ -7,9 +7,10 @@ See README for details
 @authors: Carson Lansdowne
 """
 
+import os
 import shutil
 from datetime import date, datetime, timedelta
-from rasr.util.fileio import clearFiles, makeDir
+from rasr.util.fileio import clearFiles, getListOfFiles, makeDir
 
 ########################################################
 
@@ -20,15 +21,26 @@ if __name__ == "__main__":
     today = datetime.today()
     yesterday = today - timedelta(1)
 
+    # create archive and current date folder
     folders = ["archive/", today]
     for folder in folders:
         makeDir(folder)
 
     output = ["data/", "falls/"]
 
+    # move output to current date folder and clear output
     for file in output:
         shutil.copy(file, today)
         clearFiles(file)
 
+    # copy current date folder to archive
     shutil.copy(today, "archive/")
+    # check old folder added and clear
+    yesterdaypath = "archive/" + yesterday.strftime()
+    if os.path.isfile(yesterdaypath):
+        os.remove(yesterdaypath)
+
+    # check for detections, run alert script for each one
+    detections = getListOfFiles("falls/")
+    ##alert
 
